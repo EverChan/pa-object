96d7700c581d22498091cc200f7af29767fbac13
wenyu.yfl <wenyu.yfl@taobao.com>
2014-10-07 18:32:40 +0800

    add

--- papapa/middlewares/wechat-middlewares.js
+++ papapa/middlewares/wechat-middlewares.js
@@ -9,7 +9,6 @@ var parameter = require('parameter');
 var wechat = require('koa-wechat');
 
 
-
 //初始化 koa-wechat 中间件
 module.exports = function (path,token) {
     var weChatToken = 'wechat_token';
@@ -17,16 +16,22 @@ module.exports = function (path,token) {
     return function *(next){
         var pathName=this.path;
 
-        var query=this.query;
+        var query=this.query,
+            method=this.method;
 
 
         if(path && pathName.indexOf(path)===0){
-            console.log('wechat:path:'+this.method);
-            yield wechat({ token: token||weChatToken,checksig:!query.checksig});
+            console.log('wechat:path:'+method);
+            if(method==="GET"){
+                yield wechat({ token: token||weChatToken,checksig:!query.checksig});
+            }else{
+                wechat({ token: token||weChatToken,checksig:!query.checksig});
+            }
         }
-
-        //input过程的处理结束，继续执行其他的中间件
+        
         yield next;
+
+        //yield next;//input过程的处理结束，继续执行其他的中间件
 
        //outinput
        //...
