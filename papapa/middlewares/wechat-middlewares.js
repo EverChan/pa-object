c8c3ecd18b42ddb0c246dbc30abf24489a3f1784
wenyu.yfl <wenyu.yfl@taobao.com>
2014-10-09 22:53:00 +0800

    add mount

--- papapa/middlewares/wechat-middlewares.js
+++ papapa/middlewares/wechat-middlewares.js
@@ -10,16 +10,22 @@ var mount = require('koa-mount');
 
 //初始化 koa-wechat 中间件
 module.exports = function () {
-    return function*(next){
+    return function* (next){
         var weChatToken = 'wechat_token';
         var query = this.query||{},
+            path=this.path,
             method = this.method;
         console.log('wechat:path:' + method);
-        yield mount('/wechat', wechat({
-            token: weChatToken,
-            checksig: !query.checksig,
-            session: this.session
-        }));
+
+       if(path.indexOf('/wechat')==0){
+           yield mount('/wechat', wechat({
+               token: weChatToken,
+               checksig: !query.checksig,
+               session: this.session
+           }));
+       }else{
+           yield next;
+       }
 
     }
 };
