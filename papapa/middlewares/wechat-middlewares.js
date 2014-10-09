440ef4f7fb2f39b03c10d61ac2f2b301d4dda0d6
wenyu.yfl <wenyu.yfl@taobao.com>
2014-10-09 23:49:14 +0800

    add

--- papapa/middlewares/wechat-middlewares.js
+++ papapa/middlewares/wechat-middlewares.js
@@ -6,21 +6,28 @@
 
 var wechat = require('koa-wechat');
 
+var weChatToken = 'wechat_token';
 //初始化 koa-wechat 中间件
 module.exports = function () {
-    return function * (next)
-    {
-        var weChatToken = 'wechat_token';
-        var query = this.query || {},
-            path = this.path,
-            method = this.method;
-        console.log('wechat:path:' + method, path);
-        yield wechat({
-            token: weChatToken,
-            checksig: !query.checksig,
-            session: this.session
-        });
-    }
+//    return function * (next)
+//    {
+//        var query = this.query || {},
+//            path = this.path,
+//            method = this.method;
+//        console.log('wechat:path:' + method, path);
+//        yield wechat({
+//            token: weChatToken,
+//            checksig: !query.checksig,
+//            session: this.session
+//        });
+//    }
+
+    return wechat({
+        token: weChatToken,
+        checksig: false
+    });
+
+
 };
 
 
