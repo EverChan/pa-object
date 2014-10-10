6e932dc79ec4bd868b93fc7e91b9ed6b39439433
wenyu.yfl <wenyu.yfl@taobao.com>
2014-10-10 11:54:54 +0800

    add

--- papapa/controllers/wechat/index.js
+++ papapa/controllers/wechat/index.js
@@ -18,10 +18,14 @@ exports.index = function *(){
 exports.create=function *(){
     console.log('wechat:create');
     var param=this.req.body,
+        raw=param.raw,
         query=this.query;
     console.log(param);
     this.status = 200;
     //this.body不能覆盖，所有信息放到this.req.body中。
     // koa-wechat会将req.body数据组装成xml赋给this.body返回给服务器
 //    this.req.body=param||{};
+
+    //设置相应的数据
+    this.res.body="你好："+raw.ToUserName;
 };
