4d1e9579b5351f28bc5ea2b6a9b26f0f2f099094
wenyu.yfl <wenyu.yfl@taobao.com>
2014-10-12 16:17:28 +0800

    add news

--- papapa/controllers/wechat/msg/text/index.js
+++ papapa/controllers/wechat/msg/text/index.js
@@ -8,9 +8,18 @@ var debug = require('debug')('maoxu-web:controllers:wechat');
 
 var wechat = require('wechat');
 
+var newsMsg=require('../news');
+
 
 //get /wechat
 exports.resMsg=function* (req,raw){
+    var content=raw.Content;
+
+    //如果是新闻关键字，回复新闻===
+    if(content=='news'){
+        yield newsMsg.resMsg(req,raw);
+    }
+
     var res={
         msgType: 'text',
         content: "你好，" + raw.ToUserName+'。你的消息已收到：'+raw.Content
