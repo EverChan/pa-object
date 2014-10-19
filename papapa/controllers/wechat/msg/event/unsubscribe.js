fb7b34cf6bdf829b8cab3b81a804c04e4c10421d
wenyu.yfl <wenyu.yfl@taobao.com>
2014-10-19 16:31:06 +0800

    add

--- papapa/controllers/wechat/msg/event/unsubscribe.js
+++ papapa/controllers/wechat/msg/event/unsubscribe.js
@@ -8,6 +8,7 @@ var debug = require('debug')('maoxu-web:controllers:wechat');
 
 var wechat = require('wechat');
 
+var Users = require('../../../../models/wechat_users');
 
 //get /wechat
 exports.resMsg=function* (req,raw){
