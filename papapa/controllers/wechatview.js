5bc3c43e4a2706355c5a56c7cf6692e7c83e59be
wenyu.yfl <wenyu.yfl@taobao.com>
2014-10-21 22:37:02 +0800

    add ip

--- papapa/controllers/wechatview.js
+++ papapa/controllers/wechatview.js
@@ -8,11 +8,22 @@ var debug = require('debug')('maoxu-web:controllers:wechatview');
 
 var Users = require('../models/wechat_users');
 
+
+var ip=require("ip");
+var qqwry = require('lib-qqwry').info(); //调用并初始化，普通机器初始需要70毫秒左右;
+
+
 exports.index = function* () {
 
     var usersList=yield Users.selectUsersByCity('hangzhou');
 
-    console.log(usersList);
+//    console.log(usersList);
+    var ipValue=ip.address();
+    var ipL = qqwry.searchIP(ipValue); //查询IP信息
+
+    console.log(ipL,ipValue);
+
+
 
   yield* this.render('wechatview/index.html', {
     page: 'index',
